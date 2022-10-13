b57a9c9ddddc48d8716a8fbe1ab922a4223ba3c9
Ronin <muteki.ronin@gmail.com>
2022-10-13 15:36:51 +0300

    first commit

---
+++ src/layout/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return <nav className="light-blue lighten-1">
+    <div className="nav-wrapper">
+      <a href="#!" className="brand-logo">React Shop</a>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li><a href="#!">Repo</a></li>
+      </ul>
+    </div>
+  </nav>
+
+}
+
+export default Header;
