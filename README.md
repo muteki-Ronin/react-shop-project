0b1e16f62e0ad3c80d034625af85a4152f232d27
Ronin <muteki.ronin@gmail.com>
2022-10-21 15:05:56 +0300

    add link and README

---
+++ README.md
@@ -0,0 +1,9 @@
+# **[REACT FORTNITE-SHOP PROJECT](https://github.com/muteki-Ronin/react-shop-project)**
+
+## **[React](https://ru.reactjs.org/)**
+
+## **[React "components and props"](https://ru.reactjs.org/docs/components-and-props.html)**
+
+## **[Materialize](https://materializecss.com/)**
+
+## **[FortniteApi.io](https://fortniteapi.io/)**
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,13 +1,27 @@
 function Header() {
-  return <nav className="light-blue lighten-1">
-    <div className="nav-wrapper">
-      <a href="#!" className="brand-logo">Fortnite Shop</a>
-      <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a href="#!">Repo</a></li>
-      </ul>
-    </div>
-  </nav>
-
+  return (
+    <nav className="light-blue lighten-1">
+      <div className="nav-wrapper">
+        <a
+          href="https://muteki-Ronin.github.io/react-shop-project"
+          className="brand-logo"
+        >
+          Fortnite Shop
+        </a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li>
+            <a
+              href="https://github.com/muteki-Ronin/react-shop-project"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Repo
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Header;
